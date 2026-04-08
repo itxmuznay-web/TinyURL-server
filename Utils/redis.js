1af2adcc4e0cee33c03f581c43b0d2bcaa973f18
Ahsan <thefirstblood29@gmail.com>
2026-04-08 16:03:18 +0500

    u4.2 fixed

--- Utils/redis.js
+++ Utils/redis.js
@@ -6,7 +6,7 @@ const redisClient = createClient({
   username: process.env.REDIS_USERNAME,
   password: process.env.REDIS_PASSWORD,
   socket: {
-    host: process.env.REISU_HOST,
+    host: process.env.REDIS_HOST,
     port: Number(process.env.REDIS_PORT),
   },
 });
