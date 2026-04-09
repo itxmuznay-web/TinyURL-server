ec87bf9b71f51d0e6ce88bca723b6b8711b2f2b1
Ahsan <thefirstblood29@gmail.com>
2026-04-09 15:28:56 +0500

    Update RedirectURL.js

--- Controllers/RedirectURL.js
+++ Controllers/RedirectURL.js
@@ -1,16 +1,21 @@
 import { URLs } from "../Models/url.js";
+import { getCache } from "../Utils/redis.js";
 
 export const RedirectURL = async (req, res) => {
   const { shortId } = req.params;
   try {
-    const element = await URLs.findOne({ shortId: shortId });
-    console.log(element);
-
-    if (!element) {
-      return res
-        .status(404)
-        .json({ ok: false, message: "Short URL not found" });
+    //cache  hit
+    const longURlfromCache = await getCache(shortId);
+    if (longURlfromCache) {
+      res.redirect(element.longUrl);
+      return;
     }
+
+    //cache miss
+
+    const resUrls = await URLs.find({ shortId: shortId });
+    const element = resUrls[0];
+    console.log(element);
 
     res.redirect(element.longUrl);
   } catch (err) {
